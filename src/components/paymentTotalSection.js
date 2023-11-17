eb21a4666a4dfbd5719b6ee4da9ee3c70dc132e5
PedroDaCA <pedrocal2002@gmail.com>
2023-11-16 23:58:54 -0600

    Adding payment total section

--- src/components/paymentTotalSection.js
+++ src/components/paymentTotalSection.js
@@ -1,5 +1,14 @@
-function PaymentTotalSection() {
+import { formatPriceForColonCurrency } from "../service/priceService";
 
+function PaymentTotalSection({totalToPay}) {
+    return (
+        <div>
+            Total
+            <p>Subtotal - {formatPriceForColonCurrency(totalToPay)}</p>
+            <p>Total - {formatPriceForColonCurrency(totalToPay)}</p>
+            <button disabled={totalToPay <= 0}>Checkout</button>
+        </div>
+    )
 }
 
 export default PaymentTotalSection;
