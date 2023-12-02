f8c34ff5a20ba5b17778ba6f7643566d7e2ee2e5
Esteban <labarcab820@ulacit.ed.cr>
2023-12-01 20:43:02 -0600

    Star adjustment

--- src/components/star.js
+++ src/components/star.js
@@ -1,5 +1,9 @@
 function Star({ filled }) {
-    return <span className={filled ? "star filled" : "star"}>{filled ? '★' : '☆'}</span>;
-  }
+  return (
+    <svg className={`star ${filled ? 'filled' : ''}`} width="24" height="24" viewBox="0 0 24 24">
+      <path d="M9.5 14.25l-5.584 2.936 1.066-6.218L.465 6.564l6.243-.907L9.5 0l2.792 5.657 6.243.907-4.517 4.404 1.066 6.218"/>
+    </svg>
+  );
+}
 
-  export default Star;
+export default Star;
