9a2ef7ce4b27686d85b5e7aa2ce3748361287359
Melanie Arista <melaniearista@gmail.com>
2023-12-16 09:57:14 -0600

    validaciones-pedidos

--- src/components/vendorOrderHistorySection.js
+++ src/components/vendorOrderHistorySection.js
@@ -1,74 +1,82 @@
-import VendorProduct from "./vendorProduct";
-import '../styles/vendorOrderHistory.css' ;
-import { useState } from 'react';
-import { updateOrderStatusAsync } from "../service/orderService";
+import React, { useState } from 'react';
+import VendorProduct from './vendorProduct';
+import { updateOrderStatusAsync } from '../service/orderService';
+import '../styles/vendorOrderHistory.css';
 
-function VendorOrderHistorySection({ orderList }) {
-// Se inicializa un estado llamado 'statuses' para almacenar los estados de los pedidos y una función 'setStatuses' para actualizar este estado.
-const [statuses, setStatuses] = useState({});
+function VendorOrderHistorySection({ orderList, getCurrentUser }) {
+  const [statuses, setStatuses] = useState({});
 
-// Esta función getStatusClass recibe un estado ('ready', 'pending' o 'sent') y devuelve una clase CSS correspondiente.
-const getStatusClass = (status) => {
-  switch (status) {
-    case 'ready':
-      return 'completed';
-    case 'pending':
-      return 'pending';
-    case 'sent':
-      return 'sent';
-    default:
-      return '';
-  }
-};
+  const getStatusClass = (status) => {
+    switch (status) {
+      case 'ready':
+        return 'completed';
+      case 'pending':
+        return 'pending';
+      case 'sent':
+        return 'sent';
+      default:
+        return '';
+    }
+  };
 
-// La función handleStatusChange se llama cuando se cambia el estado en un select.
-// Actualiza el estado 'statuses' con el nuevo estado del pedido en la posición index.
-const handleStatusChange = (index, status, orderId) => {
-  const updatedStatuses = { ...statuses }; // Se crea una copia del estado 'statuses'.
-  updatedStatuses[index] = status; // Se actualiza el estado del pedido en la posición 'index'.
-  setStatuses(updatedStatuses); // Se actualiza el estado 'statuses' con el nuevo estado del pedido.
-  updateOrderStatusAsync(orderId, status);
-};
+  const handleStatusChange = (index, status, orderId) => {
+    const currentUser = getCurrentUser(); // Obtener el usuario actual
+    if (!currentUser.canUpdateOrders) {
+      alert('No tienes permisos para actualizar el estado de la orden.');
+      return;
+    }
 
-// Se renderiza la lista de pedidos con la función map().
-// Para cada pedido en orderList, se crea un div que contiene la información del pedido y un select para cambiar su estado.
-return (
-  <div className="vendor-product-container">
-    {orderList?.map?.((order, index) => {
-      // Se crea un objeto 'vendorProductInformation' con los detalles del pedido para pasar al componente VendorProduct.
-      const vendorProductInformation = order.products[0];
+    const currentStatus = statuses[index] || orderList[index].state;
 
-      return (
-        <div key={index} className="order-item"> 
-          <strong className="order-id">Order ID: {order.id}</strong>
-          <VendorProduct productInformation={vendorProductInformation} />
-          <div className="order-details">
-            <p>Direccion: {order.address}</p>
-            <select
-              // El valor del select se establece según el estado del pedido en 'statuses' o el estado original del pedido.
-              value={statuses[index] || order.state} 
-              // Se llama a handleStatusChange cuando se cambia el valor del select.
-              onChange={(e) => {
-                handleStatusChange(index, e.target.value, order.id);
-              }}
-              // La clase del select se establece según el estado del pedido en 'statuses' o el estado original del pedido.
-              className={
-                statuses[index]
-                  ? getStatusClass(statuses[index]) 
-                  : getStatusClass(order.state) 
-              }
-            >
-              <option value="ready">Completado</option>
-              <option value="sent">Enviado</option>
-              <option value="pending">Pendiente</option>
-            </select>
+    if (currentStatus === 'pending' && status !== 'sent') {
+      alert('No se puede cambiar el estado de "pending" a otro estado que no sea "sent".');
+      return;
+    }
+
+    const confirmed = window.confirm('¿Estás seguro de cambiar el estado?');
+    if (!confirmed) {
+      return;
+    }
+
+    const updatedStatuses = { ...statuses };
+    updatedStatuses[index] = status;
+    setStatuses(updatedStatuses);
+    updateOrderStatusAsync(orderId, status);
+  };
+
+  return (
+    <div className="vendor-product-container">
+      {orderList?.map?.((order, index) => {
+        const vendorProductInformation = order.products[0];
+
+        return (
+          <div key={index} className="order-item">
+            <strong className="order-id">Order ID: {order.id}</strong>
+            <VendorProduct productInformation={vendorProductInformation} />
+            <div className="order-details">
+              <p>Dirección: {order.address}</p>
+              <select
+                value={statuses[index] || order.state}
+                onChange={(e) => {
+                  handleStatusChange(index, e.target.value, order.id);
+                }}
+                className={
+                  statuses[index]
+                    ? getStatusClass(statuses[index])
+                    : getStatusClass(order.state)
+                }
+              >
+                <option value="ready">Completado</option>
+                <option value="sent">Enviado</option>
+                <option value="pending">Pendiente</option>
+              </select>
+            </div>
+            <hr className="separator" />
           </div>
-          <hr className="separator" />
-        </div>
-      );
-    })}
-  </div>
-);
+        );
+      })}
+    </div>
+  );
 }
 
 export default VendorOrderHistorySection;
