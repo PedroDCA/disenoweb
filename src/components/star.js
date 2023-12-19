e8a8178cae2a154917c800bdc4235e3e77e0be62
Esteban <labarcab820@ulacit.ed.cr>
2023-12-18 21:00:58 -0600

    star adjustment
    star adjustment

--- src/components/star.js
+++ src/components/star.js
@@ -5,7 +5,7 @@ function Star({ filled, half }) {
   const halfStarPath = "M9.5 14.25l-5.584 2.936 1.066-6.218L.465 6.564l6.243-.907L9.5 0l2.792"; 
 
   return (
-    <svg className={`star ${filled ? 'filled' : ''} ${half ? 'half-filled' : ''}`} width="24" height="24" viewBox="0 0 24 24">
+    <svg width="24" height="24" viewBox="0 0 24 24">
       {half ? (
         <path d={halfStarPath} fill="currentColor" />
       ) : (
